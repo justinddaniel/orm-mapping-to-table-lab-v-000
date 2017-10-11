f37ecd51d65057aadd25338c734332d957d0868d
Justin Daniel <justinddaniel@gmail.com>
2017-10-11 02:29:42 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -35,6 +35,7 @@ class Student
     VALUES (?, ?)
     SQL
     DB[:conn].execute(sql, self.name, self.grade)
+    self
   end
 
   # Remember, you can access your database connection anywhere in this class
