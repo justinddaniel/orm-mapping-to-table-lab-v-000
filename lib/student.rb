d7920e8c1893a3ec3554734e234424ea0a61e9a8
Justin Daniel <justinddaniel@gmail.com>
2017-10-11 02:07:03 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,7 +4,7 @@ class Student
   attr_reader :id
 
   def initialize(name = nil, grade = nil)
-    
+
 
 
 
@@ -18,6 +18,6 @@ class Student
 
 
   # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
-  
+  #  with DB[:conn]
+
 end
